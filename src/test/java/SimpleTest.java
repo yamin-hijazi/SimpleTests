48c56d6eb7837391f9e7978c1f05f8be357f4c5f
mgmmgm <moshe.golan@hpe.com>
2016-11-29 11:03:09 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -20,7 +20,7 @@ public class SimpleTest {
         Assert.assertEquals(1, 1);
     }
 
-     @Test
+//      @Test
     public void alwaysFail1()
     {
         Assert.assertEquals(1, 0);
@@ -32,7 +32,7 @@ public class SimpleTest {
         Assert.assertEquals(1, 0);
     }
 
-    @Test
+//     @Test
     public void sometimesPass1()
     {
         Random random = new Random();
