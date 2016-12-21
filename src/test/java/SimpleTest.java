07477f71a6250ae8375d5bfff3e358bcc80c8b45
ido gadiel <idogadiel@gmail.com>
2016-12-21 11:24:22 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -14,13 +14,13 @@ public class SimpleTest {
        Assert.assertEquals(1, 1);
    }
 
-    @Test
+//    @Test
     public void alwaysPass2()
     {
         Assert.assertEquals(1, 1);
     }
 
-     @Test
+//     @Test
     public void alwaysFail1()
     {
         Assert.assertEquals(1, 0);
