9c6570fe5a9a2dec0f25757bef3feff16f2ac848
eemanuelhpe <eemanuel@hpe.com>
2017-02-15 11:19:21 +0200

    defect #223093 user story #224119 quality story#2241

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -38,6 +38,12 @@ public class SimpleTest {
         Assert.assertEquals(1, 2);
     }
 	
+   @Test
+    public void alwaysFail22()
+    {
+        Assert.assertEquals(1, 0);
+    }
+	
 	@Test
     public void alwaysFail3()
     {
