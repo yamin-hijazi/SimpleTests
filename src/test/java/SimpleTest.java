0d8a4df7d562643c97fec15b7db6a4108beb6c22
eemanuelhpe <eemanuel@hpe.com>
2017-03-06 16:43:02 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -25,7 +25,7 @@ public class SimpleTest {
     @Test
     public void alwaysFail1()
     {
-        Assert.assertEquals(1, 0);    }
+        Assert.assertEquals(1, 1);    }
 
      @Test
     public void alwaysFail2()
@@ -33,19 +33,19 @@ public class SimpleTest {
         Assert.assertEquals(1, 0);
     }
 	
-    @Test
+   // @Test
     public void alwaysFail33()
     {
         Assert.assertEquals(1, 0);
     }
 	
-   @Test
+  // @Test
     public void alwaysFail334()
     {
         Assert.assertEquals(1, 2);
     }
 	
-   @Test 
+ //  @Test 
     public void alwaysFail22()
     {
         Assert.assertEquals(1, 0);
