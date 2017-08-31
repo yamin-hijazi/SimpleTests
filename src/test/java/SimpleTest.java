b62ddfe82f5b3e6256c323c5d6c5c3c22029e50b
ido gadiel <idogadiel@gmail.com>
2017-08-31 14:47:04 +0300

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -49,8 +49,8 @@ public class SimpleTest {
         Random random = new Random();
         int randomNumber = random.nextInt(100);
         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-    //    Assert.assertEquals(smallerThanFiftee, true);
-	    Assert.assertEquals(1,2);
+        Assert.assertEquals(smallerThanFiftee, true);
+   
     }
 	
 	@Test
