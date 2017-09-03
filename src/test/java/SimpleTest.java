e9ecacd450382ec44a55264cea70e0013e55b649
ido gadiel <idogadiel@gmail.com>
2017-09-03 13:15:23 +0300

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,7 +1,6 @@
 import junit.framework.Assert;
-import org.junit.Test;
 import java.util.Random;
-import org.junit.Ignore;
+import org.junit.*;
                                                                                     
 /**                                    
  *  Created by gadiel on 15/11/2016.                            
