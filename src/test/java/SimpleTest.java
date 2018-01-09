b0d176e24699ee35c06ab573e3bd21d80f2933de
ido gadiel <idogadiel@gmail.com>
2018-01-09 16:45:33 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -6,7 +6,7 @@ import java.util.Random;
                                                                                        
 /**                                         
  *  Created by gadiel on 15/11/2016.                   
- */
+ */ 
 
 // commit 1
                              
