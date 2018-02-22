3c326fb4b601e907ad1902831695f8a17eb003a8
mgmmgm <moshe.golan@hpe.com>
2018-02-22 10:20:02 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -36,7 +36,12 @@ public class SimpleTest {
     {
         Assert.assertEquals(1, 2);
     }
-	      
+
+	@Test
+    public void alwaysFail3()
+    {
+        Assert.assertEquals(1, 2);   
+    }   
  
     @Test
     public void sometimesPass1()
