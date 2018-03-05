7dacd75e8247ee5bf9c2c62c01dd4852be64e86a
mgmmgm <moshe.golan@hpe.com>
2018-03-05 16:59:56 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -36,25 +36,14 @@ public class SimpleTest {
     {
         Assert.assertEquals(1, 2);
     }
+	
+	@Test
+    public void alwaysFail3()
+    {
+        Assert.assertEquals(1, 2);
+    }
 	      
  
-    @Test
-    public void sometimesPass1()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-    }
-
-    @Test  
-    public void sometimesPass2()
-    {
-        Random random = new Random();
-        int randomNumber = random.nextInt(100);
-        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
-        Assert.assertEquals(smallerThanFiftee, true);
-    }
 	
 	@Test
 	@Ignore
