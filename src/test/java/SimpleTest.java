f71d54a6bf80373d599688978a92b13b283b8ed6
yamin-hijazi <41009903+yamin-hijazi@users.noreply.github.com>
2019-03-19 17:37:53 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -71,6 +71,14 @@ public class SimpleTest {
         boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
         Assert.assertEquals(smallerThanFiftee, true);
     }
+    @Test  
+    public void sometimesPass5()
+    {
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        boolean smallerThanFiftee =  randomNumber < 50 ? true : false;
+        Assert.assertEquals(smallerThanFiftee, true);
+    }
 	
 	@Test
 	@Ignore
